52e384d168a74e53fffd71cd1c0a9307d9944490
Serenechien0172 <serenechien0172@gmail.com>
2025-11-11 12:53:21 -0500

    fix: resolve merge conflict in NavBar

--- weather-wear-frontend/src/components/NavBar/NavBar.tsx
+++ weather-wear-frontend/src/components/NavBar/NavBar.tsx
@@ -1,6 +1,6 @@
 import '../NavBar/NavBar.css';
-import { FaUser , FaHome,FaTshirt} from "react-icons/fa";
-import {useState} from "react";
+import { FaUser, FaHome, FaTshirt } from "react-icons/fa";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function Sidebar() {
@@ -16,10 +16,14 @@ export default function Sidebar() {
             <div className={`sidebar-dropdown ${open ? 'open' : ''}`}>
                 <ul>
                     <li onClick={() => navigate("/")}>
-                        <FaHome size={15} color="white"/>{" "} Home
+                        <FaHome size={15} color="white" />{" "} Home
                     </li>
-                    <li><FaUser size={15} color="white"/>{" "} Profile</li>
-                    <li><FaTshirt size={15} color="white"/>{" "} Wardrobe</li>
+                    <li>
+                        <FaUser size={15} color="white" />{" "} Profile
+                    </li>
+                    <li>
+                        <FaTshirt size={15} color="white" />{" "} Wardrobe
+                    </li>
                 </ul>
             </div>
         </div>
